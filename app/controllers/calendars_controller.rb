662039e8b34cd3060cac7bf9536ea60316364e66
tadat017 <tadat017@gmail.com>
2024-06-13 16:57:21 +0900

    5

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,10 +8,14 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    @plan = Plan.new(plan_params)
+    if @plan.save
     redirect_to action: :index
+   else
+    get_week
+    render :index
+   end
   end
-
   private
 
   def plan_params
